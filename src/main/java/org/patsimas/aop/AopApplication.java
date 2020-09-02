8eaf11ef7b38b6ceab23fa4a38d4e7efee5b814a
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2020-09-02 17:48:03 +0300

    first commit

---
+++ src/main/java/org/patsimas/aop/AopApplication.java
@@ -0,0 +1,16 @@
+package org.patsimas.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApplication.class, args);
+	}
+
+//	https://www.javatpoint.com/spring-boot-aop
+//	https://www.javatpoint.com/spring-aop-aspectj-annotation-example
+
+}
---
+++ src/test/java/org/patsimas/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.patsimas.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
